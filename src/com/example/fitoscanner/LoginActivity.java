f6b41a92d2038d3196dccd538f6693e127fd6fe0
eiroa <eiroa@gmail.com>
2014-05-15 03:45:06 -0300

    1. Layout Inicial de Login 2. Color xml agregado junto a imagen planta

--- src/com/example/fitoscanner/LoginActivity.java
+++ src/com/example/fitoscanner/LoginActivity.java
@@ -16,6 +16,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //
 
       
     }
