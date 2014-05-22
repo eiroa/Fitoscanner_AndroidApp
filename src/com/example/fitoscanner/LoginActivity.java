1fcc8c802afbe99481fe069505d198fe817510c1
eiroa <eiroa@gmail.com>
2014-05-22 14:42:53 -0300

    1. Se Agregan accesos directos a Actividades de muestras y tomar muestra 2. Se agrega la posibilidad de cambiar la fuente por una personalizada 3. Se agregan permisos en Manifest

--- src/com/example/fitoscanner/LoginActivity.java
+++ src/com/example/fitoscanner/LoginActivity.java
@@ -1,23 +1,65 @@
 package com.example.fitoscanner;
 
 
+
+import com.example.fitoscanner.helpers.TypefacesHelper;
+
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.util.Log;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class LoginActivity extends Activity {
-
+public class LoginActivity extends Activity implements OnClickListener{
+	LoginActivity activity;
+	
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //
+        this.activity = this;
+        TextView txtView1 = (TextView) findViewById(R.id.loginUserText);
+        TextView txtView2 = (TextView) findViewById(R.id.loginPassText);
+        Button bt = (Button) findViewById(R.id.loginButton);
+        Typeface font = TypefacesHelper.getTypeface(this, "fonts/optien.ttf");
+        txtView1.setTypeface(font);
+        txtView2.setTypeface(font);
+        bt.setTypeface(font);
+        
+        bt.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View arg0) {
+            	//Toast.makeText(activity,"heyyy",Toast.LENGTH_SHORT).show();
+        		
+        		EditText txtView1 = (EditText) findViewById(R.id.userField);
+        		EditText txtView2 = (EditText) findViewById(R.id.passField);
+        		String user = (txtView1.getText()).toString();
+                String pass = (txtView2.getText()).toString();
+        		if(user.equals("admin")&&pass.equals("1234")){
+        			Toast.makeText(activity.getApplicationContext(),"Se ha logueado correctamente",Toast.LENGTH_LONG).show();
+        			Intent intent = new Intent(activity, MenuActivity.class);
+        	        //intent.putExtra(GenericNames.CATEGORY_ID, categoryId);
+        	        activity.startActivity(intent);
+        		}else{
+        			Toast.makeText(activity.getApplicationContext(),"Usuario y/o clave incorrecta",Toast.LENGTH_LONG).show();
+        		}
+
+            }
+        });
+        //
       
     }
 
@@ -27,6 +69,7 @@ public class LoginActivity extends Activity {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
+        TextView txtView2 = (TextView) findViewById(R.id.loginPassText);
         return true;
     }
 
@@ -41,6 +84,20 @@ public class LoginActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	public void onClick(View v) {
+		
+		
+		
+	}
+
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
 
    
 }
--- src/com/example/fitoscanner/MenuActivity.java
+++ src/com/example/fitoscanner/MenuActivity.java
@@ -1,7 +1,54 @@
 package com.example.fitoscanner;
 
+import com.example.fitoscanner.helpers.TypefacesHelper;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends Activity {
+	MenuActivity activity;
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+        this.activity = this;
+        TextView txtView1 = (TextView) findViewById(R.id.textView1);
+        TextView txtView2 = (TextView) findViewById(R.id.textView2);
+        Button bt = (Button) findViewById(R.id.buttonMenuLogout);
+        Typeface font = TypefacesHelper.getTypeface(this, "fonts/optien.ttf");
+        txtView1.setTypeface(font);
+        txtView2.setTypeface(font);
+        bt.setTypeface(font);
+        bt.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View arg0) {       		
+        		activity.finish();
+            	Toast.makeText(activity.getApplicationContext(),"Se ha desconectado",Toast.LENGTH_LONG).show();       		
+            }
+        });
+        
+        ImageView arrowRegisters = (ImageView)findViewById(R.id.imMenuArrowRegisters);
+        arrowRegisters.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+            	Intent intent = new Intent(activity, RecordsActivity.class);
+        		activity.startActivity(intent);      		
+            }
+        });
+        ImageView arrowTakePic = (ImageView)findViewById(R.id.imMenuArrowTakePic);
+        arrowTakePic.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+            	Intent intent = new Intent(activity, ImageActivity.class);
+        		activity.startActivity(intent);      		
+            }
+        });
+    }
 }
