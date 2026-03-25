f4ac55294f5dd98369053921351e31ef1c8134f6
mauroeiroa <mauro.eiroa@ey.com>
2026-03-24 22:54:11 -0300

    url helper

--- src/ar/edu/unq/fitoscanner/helpers/URLHelper.java
+++ src/ar/edu/unq/fitoscanner/helpers/URLHelper.java
@@ -3,6 +3,9 @@ package ar.edu.unq.fitoscanner.helpers;
 import android.R.string;
 
 public class URLHelper { 
-	public static final String SERVER_ADDRESS = "http://104.236.252.51:8080/tip_eiroa_mauro_server_backend/rest";
-//	public static final String SERVER_ADDRESS = "http://192.168.137.213:8081/tip_eiroa_mauro_server_backend/rest";
+	// Remote production server
+//	public static final String SERVER_ADDRESS = "http://104.236.252.51:8080/tip_eiroa_mauro_server_backend/rest";
+
+	// Local development server via ADB reverse port forwarding (adb reverse tcp:8081 tcp:8081)
+	public static final String SERVER_ADDRESS = "http://127.0.0.1:8081/tip_eiroa_mauro_server_backend/rest";
 }
